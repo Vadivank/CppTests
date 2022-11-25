e6329ab9a6d5d210f95f7d3daf0692c5fbcb851e
Vadivank <68869494+Vadivank@users.noreply.github.com>
2022-11-25 18:41:59 +0300

    add new method for socket init (and stay legacy method)

--- TCPMinimalChat/server/server.cpp
+++ TCPMinimalChat/server/server.cpp
@@ -2,150 +2,96 @@
 #pragma comment(lib, "ws2_32.lib");
 #pragma warning(disable : 4996)
 
-#define WIN(exp) exp  // быстрая обвязка макросами участков кода под ОС
-#define NIX(exp)
-
-#include <winsock2.h>
-
-#ifdef __WIN32
-#define _WINSOCK_DEPRECATED_NO_WARNINGS  // для обхода некоторых ошибок
-#include <windows.h>
-#include <ws2tcpip.h>
-
-#include <ctime>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <string>
 #include <thread>
 #include <vector>
 
-// #include "common.h"
+#include "../../mysocket.h"
 
-// bbej
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <netdb.h>
-// #include <arpa/inet.h>
-// #include <sys/wait.h>
-#include <signal.h>
-// bbej
-
-#ifndef __WIN32
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#include <stdexcept>
-#endif
-
-#endif
-
-#ifdef __linux__
-// #include <cstdlib>
-// #include <cstring>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-#endif
+#define WIN(exp) exp  // быстрая обвязка макросами участков кода под ОС
+#define NIX(exp)
+#define _WINSOCK_DEPRECATED_NO_WARNINGS  // для обхода некоторых ошибок
+#define BACKLOG 10  // размер очереди ожидающих подключений
 
 const int AMOUNT_CONNECTIONS = 10;
 SOCKET connections[AMOUNT_CONNECTIONS];
-#define BACKLOG 10  // размер очереди ожидающих подключений
 
 /* тип пакета данных позволяет идентифицировать данные */
 enum Packet { P_CHAT_MESSAGE, P_TEST };
+bool processPacket(GetPeerIP &gpip, int index, Packet packetType);
+void clientHandler(int index);
+void connectionInit(const char *address, const char *port);
 
-/* позволит вывести IP и PORT в удобном для четния виде */
-struct GetPeerIP {
-   private:
-    SOCKET m_socket;
-    const short portStrSize{sizeof(unsigned short) + 1};  // 65535 + '\0'
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout << "Usage: [app] [ip] [port]" << std::endl;
+        Sleep(2000);
+        return 1;
+    }
 
-   public:
-    std::string ipStr;
-    char ipChar[INET_ADDRSTRLEN];
+    std::cout << "Server: " << argv[1] << ":" << argv[2] << std::endl;
 
-    unsigned short port;
-    std::string portStr;
-    char *portChar = new char[portStrSize];
+    connectionInit(argv[1], argv[2]);
 
-    std::string ipMessage;
+    // connectionInitLegacy(argv[1], std::stoi(std::string(argv[2])));  //
+    // legacy
 
-    int errorState;
-    std::string errorMsg;
+    return 0;
+}
+/*
 
-    GetPeerIP(SOCKET peerSocket) : m_socket(peerSocket) {
-        sockaddr_in t_sock;
-        socklen_t t_len = sizeof(t_sock);
-        memset(&t_sock, 0, t_len);
 
-        if ((errorState =
-                 getpeername(peerSocket, (sockaddr *)&t_sock, &t_len))) {
-            errorMsg = "GetPeerIP error #" + std::to_string(errorState);
+
+
+
+
+*/
+/* обработчик событий на каждое соединение - живет в отдельном потоке */
+void clientHandler(int index) {
+    Packet packetType;
+    GetPeerIP gpip{GetPeerIP(connections[index])};
+    std::cout << "Connected #" << index << " "
+              << GetPeerIP(connections[index]).getPeerName() << std::endl;
+    int recvBytes{0};
+
+    while (true) {
+        recvBytes =
+            recv(connections[index], (char *)&packetType, sizeof(Packet), 0);
+
+        if (recvBytes < 0) {
+            break;
         } else {
-            errorMsg = "OK";
+            std::cout << "=========================\n"
+                      << "Received data packet from client #" << index << " "
+                      << gpip.ipMessage << " ";
         }
 
-        ipStr = inet_ntoa(t_sock.sin_addr);
-        strncpy(ipChar, ipStr.c_str(), INET_ADDRSTRLEN);
+        switch (packetType) {
+            case P_CHAT_MESSAGE: {
+                std::cout << "[Chat message] ";
+                if (!processPacket(gpip, index, packetType)) {
+                    std::cout << "cH: unrecognized message!\n";
+                    break;
+                }
+                break;
+            }
+            case P_TEST: {
+                std::cout << "[Test message]\n";
+                break;
+            }
+            default: {
+                std::cout << "[Undefined message]\n";
+                break;
+            }
+        }
 
-        unsigned short port(ntohs(t_sock.sin_port));
-        portStr = std::to_string(port);
-        strncpy(portChar, portStr.c_str(), portStrSize);
-
-        ipMessage = "(IP: " + ipStr + ":" + portStr + ")";
+        if (isClosedSocket(connections[index], {0, 10000})) {
+            std::cout << "cH: client #" << index << " not ready-to-read\n";
+            break;
+        }
     }
-
-    ~GetPeerIP() { delete[] portChar; }
-};
-
-/* сокет готов к четнию? <timeOut> - время ожидания ответа от сокета */
-bool isClosedSocket(SOCKET sock, timeval timeOut = {0, 0}) {
-    fd_set rfd;
-    FD_ZERO(&rfd);
-    FD_SET(sock, &rfd);
-
-    if ((timeOut.tv_sec == -1) || (timeOut.tv_usec == -1)) {
-        /* вернуть rfd, как только в нем, что-то появится */
-        select(sock + 1, &rfd, 0, 0, NULL);
-    } else {
-        /* ждать наполнение rfd в течение timeOut */
-        select(sock + 1, &rfd, 0, 0, &timeOut);
-    }
-    /* проверить rfd на наличие в нем sock*/
-    if (!FD_ISSET(sock, &rfd)) {
-        return false;
-    }
-    NIX(int n = 0; ioctl(sock, FIONREAD, &n); return n == 0;)
-    return true;
-}
-
-/* текущее время */
-std::ostringstream currentTime() {
-    std::time_t t_time{std::time(nullptr)};
-    std::tm *tm{std::localtime(&t_time)};
-    std::ostringstream oss;
-
-    oss << tm->tm_mday << "." << tm->tm_mon << "." << tm->tm_year + 1900 << " "
-        << tm->tm_hour << ":";
-    if (tm->tm_min > 10)
-        oss << tm->tm_min << ":";
-    else
-        oss << "0" << tm->tm_min << ":";
-    if (tm->tm_sec > 10)
-        oss << tm->tm_sec << " ";
-    else
-        oss << "0" << tm->tm_sec << " ";
-    return oss;
+    std::cout << "Disconnected #" << index << " " << gpip.getPeerName() << " "
+              << gpip.ipMessage << std::endl;
+    closesocket(connections[index]);
 }
 
 /* логика обработки сообщения */
@@ -213,56 +159,111 @@ bool processPacket(GetPeerIP &gpip, int index, Packet packetType) {
     return true;
 }
 
-/* обработчик событий на каждое соединение - живет в отдельном потоке */
-void clientHandler(int index) {
-    Packet packetType;
-    GetPeerIP gpip{GetPeerIP(connections[index])};
-    std::cout << "Client #" << index << " CONNECTED" << std::endl;
-    int recvBytes{0};
-
-    while (true) {
-        recvBytes =
-            recv(connections[index], (char *)&packetType, sizeof(Packet), 0);
-
-        if (recvBytes < 0) {
-            break;
-        } else {
-            std::cout << "=========================\n"
-                      << "Received data packet from client #" << index << " "
-                      << gpip.ipMessage << " ";
-        }
-
-        switch (packetType) {
-            case P_CHAT_MESSAGE: {
-                std::cout << "[Chat message] ";
-                if (!processPacket(gpip, index, packetType)) {
-                    std::cout << "cH: unrecognized message!\n";
-                    break;
-                }
-                break;
-            }
-            case P_TEST: {
-                std::cout << "[Test message]\n";
-                break;
-            }
-            default: {
-                std::cout << "[Undefined message]\n";
-                break;
-            }
-        }
-
-        if (isClosedSocket(connections[index], {0, 10000})) {
-            std::cout << "cH: client #" << index << " not ready-to-read\n";
-            break;
-        }
+/* инициализация сервера */
+void connectionInit(const char *address, const char *port) {
+    /* WSA init */
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 2);
+    if (WSAStartup(DLLVersion, &wsaData)) {
+        std::cout << "Error #" << WSAGetLastError()
+                  << ": Application can not load WSA library!" << std::endl;
+        exit(1);
+    } else {
+        std::cout << "WSA initialization is OK" << std::endl;
     }
-    std::cout << "Client #" << index << " " << gpip.ipMessage << " closed!"
-              << std::endl;
-    closesocket(connections[index]);
+
+    int sockfd;                            // socket declaration
+    int es;                                // error ststus
+    struct addrinfo hints, *servinfo, *p;  // structs for address information
+    memset(&hints, 0, sizeof hints);       // clean temporary struct
+    hints.ai_family = PF_UNSPEC;           // automatic socket type detection
+    hints.ai_socktype = SOCK_STREAM;       // TCP
+    // hints.ai_flags = AI_NUMERICHOST;       // numeric value for address
+
+    /* filling the structure with address data */
+    if (getaddrinfo(address, port, &hints, &servinfo)) {
+        perror("Getaddrinfo ERROR");
+        exit(1);
+    }
+
+    /* loop through all available addresses and bind to the first possible one
+     */
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) ==
+            -1) {
+            perror("Socket initialisation ERROR");
+            continue;
+        }
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            closesocket(sockfd);
+            perror("Socket binding ERROR");
+            continue;
+        }
+        printf("Client bind to %s\n",
+               inet_ntoa(((sockaddr_in *)(p->ai_addr))->sin_addr));
+        break;
+    }
+    if (p == NULL) {
+        perror("Failed to bind socket");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    /* listening */
+    int errorState = listen(sockfd, SOMAXCONN);
+    if (errorState != 0) {
+        std::cout << "Error #" << WSAGetLastError()
+                  << ": incorrect start listening!" << std::endl;
+        closesocket(sockfd);
+        WSACleanup();
+        exit(1);
+    } else {
+        char name[255];
+        gethostname(name, 255);
+        name[255] = '\0';
+        std::cout << "\nHostname: " << name << " - ";
+        std::cout << "Listening connections..." << std::endl;
+    }
+
+    /* loop connections */
+    std::vector<std::thread> clientThreads;
+    for (int i = 0; i < AMOUNT_CONNECTIONS; ++i) {
+        int sizeofaddr = sizeof(sockaddr);
+
+        SOCKET newConnection =
+            accept(sockfd, (sockaddr *)&p->ai_addr, &sizeofaddr);
+
+        if (newConnection == INVALID_SOCKET) {
+            std::cout << "Error #" << WSAGetLastError() << ": client #" << i
+                      << " can not connect to server!" << std::endl;
+            closesocket(sockfd);
+            closesocket(newConnection);
+            WSACleanup();
+            exit(1);
+        } else {
+            std::string msg = "\nClient #" + std::to_string(i) + " " +
+                              GetPeerIP(newConnection).getPeerName() +
+                              ", welcome to server!\n";
+            int msgSize = msg.size();
+            Packet msgType = P_CHAT_MESSAGE;
+            send(newConnection, (char *)&msgType, sizeof(Packet), 0);
+            send(newConnection, (char *)&msgSize, sizeof(int), 0);
+            send(newConnection, msg.c_str(), msgSize, 0);
+            /* Отправка клиенту тестового сообщения */
+            // Packet testPacket = P_TEST;
+            // send(newConnection, (char *)&testPacket, sizeof(Packet), 0);
+        }
+        connections[i] = newConnection;
+        clientThreads.emplace_back(clientHandler, i);
+    }
+    for (int i = 0; i < AMOUNT_CONNECTIONS; ++i) {
+        clientThreads[i].join();
+    }
 }
 
-/* инициализация сервера */
-void connectionInit(const char *address, unsigned short port) {
+/* Legacy инициализация сервера */
+void connectionInitLegacy(const char *address, unsigned short port) {
     /* WSA init */
     WSAData wsaData;
     WORD DLLVersion = MAKEWORD(2, 2);
@@ -340,8 +341,8 @@ void connectionInit(const char *address, unsigned short port) {
             WSACleanup();
             exit(1);
         } else {
-            std::string msg = "\nClient #" + std::to_string(i) +
-                              GetPeerIP(newConnection).ipMessage +
+            std::string msg = "\nClient #" + std::to_string(i) + " " +
+                              GetPeerIP(newConnection).getPeerName() +
                               ", welcome to server!\n";
             int msgSize = msg.size();
             Packet msgType = P_CHAT_MESSAGE;
@@ -358,19 +359,4 @@ void connectionInit(const char *address, unsigned short port) {
     for (int i = 0; i < AMOUNT_CONNECTIONS; ++i) {
         clientThreads[i].join();
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cout << "Need ip address: <app> <ip> <port>" << std::endl;
-        Sleep(2000);
-        return 1;
-    }
-
-    std::cout << "Server\nIP: " << argv[1] << "\t Port: " << argv[2]
-              << std::endl;
-
-    connectionInit(argv[1], std::stoi(std::string(argv[2])));
-
-    return 0;
 }
